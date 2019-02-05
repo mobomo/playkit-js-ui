3f2d6fdcbbe71c2b7a48723c59ccd1383c3641ac
Jason Murray <jason.murray@mobomo.com>
2019-02-05 16:27:01 -0500

    Remove presets duplicated in kaltura player

--- src/ui-presets/index.js
+++ src/ui-presets/index.js
@@ -1,5 +1,1 @@
-export {idleUI} from './idle';
-export {playbackUI} from './playback';
-export {adsUI} from './ads';
-export {errorUI} from './error';
-export {liveUI} from './live';
+export {voaAudioUi} from './voa-audio-playback';
